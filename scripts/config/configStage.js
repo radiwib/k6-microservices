522e8159d6d3b0ad5a11cbaac6229640b55d59d7
rahadian wibisono <radi.wib@gmail.com>
2025-06-12 16:19:24 +0700

    commit

--- scripts/config/configStage.js
+++ scripts/config/configStage.js
@@ -1,20 +1,17 @@
-export const USER_URL = `https://ionuser-s.${__ENV.BASE_URL}`;
-export const BIKE_URL = `https://ionbikes-s.${__ENV.BASE_URL}`;
-export const NOTIFICATION_URL = `https://notifications-s.${__ENV.BASE_URL}`;
 export const ENDPOINTS = {
-  getBikes: `${BIKE_URL}/api/v2/bikes`,
-  getUser: `${USER_URL}/api/v2/users`,
-  getNotifications: `${NOTIFICATION_URL}/api/v2/notifications`,
-  getUserById: (id) => `${USER_URL}/api/v2/users/${id}`,
-  getBikeById: (bike_id) => `${BIKE_URL}/api/v2/bikes/${bike_id}`,
-  login: `${USER_URL}/api/v2/request-login`,
-  verifyLogin: `${USER_URL}/api/v2/verify-login`,
+  getBikes: `${__ENV.BIKE_URL}/api/v2/bikes`,
+  getUser: `${__ENV.USER_URL}/api/v2/users`,
+  getNotifications: `${__ENV.NOTIFICATION_URL}/api/v2/notifications`,
+  getUserById: (id) => `${__ENV.USER_URL}/api/v2/users/${id}`,
+  getBikeById: (bike_id) => `${__ENV.BIKE_URL}/api/v2/bikes/${bike_id}`,
+  login: `${__ENV.USER_URL}/api/v2/request-login`,
+  verifyLogin: `${__ENV.USER_URL}/api/v2/verify-login`,
 };
 export const ACCESS_TOKEN = __ENV.ACCESS_TOKEN
-export const USER_CREDENTIALS = {
-  username: __ENV.PHONE || '6281122334455',
-  password: __ENV.TYPE || 'whatsapp',
+export const LOGIN = {
+  identity: __ENV.PHONE || '6281122334455',
+  type: __ENV.TYPE || 'whatsapp',
 };
-export const USER_VERIFY = {
+export const VERIFY = {
   code: __ENV.CODE || '123456',
 };
