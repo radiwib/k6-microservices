f37f404cb9513d19322735100e123c1f476852fc
rahadian wibisono <radi.wib@gmail.com>
2025-06-13 16:17:48 +0700

    commit

---
+++ k6.config.js
@@ -0,0 +1,13 @@
+export const options = {
+  stages: [
+    { duration: '30s', target: 20 },   // ramp up to 20 VUs
+    { duration: '1m', target: 20 },    // hold at 20 VUs
+    { duration: '30s', target: 40 },   // spike to 40 VUs
+    { duration: '1m', target: 40 },    // hold at 40 VUs
+    { duration: '30s', target: 0 },    // ramp down
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // 95% requests should be < 500ms
+    http_req_failed: ['rate<0.01'],   // < 1% requests should fail
+  },
+};
